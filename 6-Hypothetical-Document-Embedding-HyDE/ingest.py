cac2e6dd25073b9eded52f732797a2a8c566abe3
Richa-chauhan2905 <chauhanricha2911@gmail.com>
2025-05-28 12:06:02 +0530

    hypothetical-document-embedding

---
+++ 6-Hypothetical-Document-Embedding-HyDE/ingest.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_qdrant import QdrantVectorStore
+from embedder import get_embedder
+
+
+def ingest_pdf_to_qdrant():
+    pdf_file_path = Path(__file__).parent / "data" / "nodejs.pdf"
+    loader = PyPDFLoader(file_path=pdf_file_path)
+    docs = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200,
+    )
+    split_docs = text_splitter.split_documents(documents=docs)
+
+    embedder = get_embedder()
+    vector_store = QdrantVectorStore.from_documents(
+        documents=[],
+        url="http://localhost:6333",
+        collection_name="hyde",
+        embedding=embedder,
+    )
+    vector_store.add_documents(documents=split_docs)
+
+    print("Ingested successfully")
+
+
+if __name__ == "__main__":
+    ingest_pdf_to_qdrant()
---
+++ 6-Hypothetical-Document-Embedding-HyDE/main.py
@@ -0,0 +1,20 @@
+from HyDE import hyde, get_final_answer
+
+def main():
+    while True:
+        query = input("> ")
+        if query.lower() == "clear":
+            break
+
+        retrieved_chunks = hyde(query)
+
+        if not retrieved_chunks:
+            print("No relevant results found")
+        else:
+            final_ans = get_final_answer(query, retrieved_chunks)
+
+            print(f"\nRelevant Result:\n{final_ans}")
+
+
+if __name__ == "__main__":
+    main()
